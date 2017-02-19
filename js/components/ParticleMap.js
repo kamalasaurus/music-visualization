789883b5c359ba49bc3a77ac6d4c502ff7bb9101
kamalasaurus <kamalasaurus@gmail.com>
2017-02-19 02:23:45 -0500

    cleanup

--- js/components/ParticleMap.js
+++ js/components/ParticleMap.js
@@ -1,29 +1,33 @@
 import * as THREE from 'three';
 
 export default class ParticleMap {
-  constructor(scene, camera, renderer) {
+  constructor() {
     //console.log(THREE);
-    this.scene = scene;
-    this.camera = camera;
-    this.renderer = renderer;
+
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    document.body.appendChild(renderer.domElement);
 
     const geometry = new THREE.BoxGeometry(1, 1, 1);
     const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
     const cube = new THREE.Mesh(geometry, material);
-    this.scene.add(cube);
 
-    this.camera.position.z = 5;
+    scene.add(cube);
+    camera.position.z = 5;
 
-    this.render = () => {
-      requestAnimationFrame(this.render);
+    const render = () => {
+      requestAnimationFrame(render);
 
       cube.rotation.x += 0.05;
       cube.rotation.y += 0.05;
 
-      this.renderer.render(this.scene, this.camera);
+      renderer.render(scene, camera);
     };
 
-    this.render();
+    render();
   }
 }
 
